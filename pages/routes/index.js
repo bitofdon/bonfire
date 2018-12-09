d59d645213f9dc8cccaf4295c03ff3aa024cd932
don <bitofdon@Dons-MacBook-Pro.local>
2018-12-09 16:29:16 -0500

    fixed heroku files

--- pages/routes/index.js
+++ pages/routes/index.js
@@ -1,10 +1,6 @@
 var express = require('express');
 var router = express.Router();
 
-/* needed for Heroku */
-app.set('port', (process.env.PORT || 5000));
-
-
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('home', { 
